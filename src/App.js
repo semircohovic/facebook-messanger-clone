23c8516950e72e64f3eea3f987844b674a42304b
Semir Cohovic <semircohovic@gmail.com>
2020-08-04 16:39:05 +0200

    printig messages

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['hello', 'staovo']);
+
+  console.log(messages);
+  // console.log(input);
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    setInput('');
+  }
   return (
     <div className="App">
        <h1>Test!!</h1>
 
-       <input />
-       <button> Send message </button>
+       <input value={input} onChange={event => setInput(event.target.value)}/>
+       
+       <button onClick={sendMessage}> Send message </button>
     </div>
   );
 }
